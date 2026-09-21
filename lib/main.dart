a0804ade0d5e0ee8a865f821243973e0e2b07839
mewmyauu <rk.vladislav@gmail.com>
2026-09-21 16:45:33 +0300

    vtoroe zadanie

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: (Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.white,
-                Colors.blue,
-                Colors.red,
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              "Hello World!",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-              ),
-            ),
-          ),
+        body: Text(
+          "Привет! Меня зовут Владислав.\n Я студент группы ИСП-243.",
         ),
       )),
     ),
